4946c640e624cee1d37161463a74aa38cf3b1324
ClauMolinari <148367813+ClauMolinari@users.noreply.github.com>
2023-10-18 17:37:34 -0300

    Update Tabela Tipo Matricula.sql

--- Tabela Tipo Matricula.sql
+++ Tabela Tipo Matricula.sql
@@ -1,7 +1,7 @@
 -- adicionando registros em tabela tipo_matricula
 
-insert into tipo_matricula (cod_tipo_matricula,descr_tipo_matricula)
-  values
+INSERT INTO tipo_matricula (cod_tipo_matricula,descr_tipo_matricula)
+  VALUES
   (1,'Estudante'),
   (2,'Professor'),
   (3,'Funcionário'),
