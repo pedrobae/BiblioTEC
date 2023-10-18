ed91034b4732648192900f4ab2546f1f1cea21ad
ClauMolinari <148367813+ClauMolinari@users.noreply.github.com>
2023-10-18 18:09:20 -0300

    Update Tabela Autor.sql

--- Tabela Autor.sql
+++ Tabela Autor.sql
@@ -2,15 +2,42 @@
 
 INSERT INTO autor (cod_autor,nome_autor,pais_autor)
 	VALUES
-	(1,'XXX','Brasil'),
-	(2,'XXX','Brasil'),
-	(3,'XXX','Brasil'),
-	(4,'XXX','Brasil'),
-	(5,'XXX','Brasil'),
-	(6,'XXX','Brasil'),
-	(7,'XXX','Brasil'),
-	(8,'XXX','Brasil'),
-	(9,'XXX','Brasil'),
-	(10,'XXX','Brasil');
+	(1,'Robert C. Martin ','Estados Unidos'),
+	(2,'Andrew Hunt','Inglaterra'),
+	(3,'David Thomas','Inglaterra'),
+	(4,' Yuval Noah Harari','Israel'),
+	(5,'Walter Isaacson','Estados Unidos'),
+	(6,'Eric Matthes','Estados Unidos'),
+	(7,'Erich Gamma','Suiça'),
+	(8,'Richard Helm','Estados Unidos'),
+	(9,' Ralph Johnson','Estados Unidos'),
+	(10,'Gene Kim','Estados Unidos'),
+	(11,'Kevin Behr','Estados Unidos'),
+	(12,'George Spafford','Estados Unidos'),
+	(13,'Karina Vazquez','Brasil'),
+	(14,'Mark Manson','Estados Unidos'),
+	(15,'Andreas M. Antonopoulos','Brasil'),
+	(16,'Charles Duhigg','Mexico'),
+	(17,'Timothy Ferriss','Estados Unidos'),
+	(18,'Carol S. Dweck ','Estados Unidos'),
+	(19,'Robert T. Kiyosaki','Estados Unidos'),
+	(20,'Mark Manson','Estados Unidos'),
+	
+	
+	
 
-		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
