17e067d325cc51c5e21d3e91f57c38b358d32a83
ClauMolinari <148367813+ClauMolinari@users.noreply.github.com>
2023-10-18 17:40:04 -0300

    Update Tabela Autor.sql

--- Tabela Autor.sql
+++ Tabela Autor.sql
@@ -1,16 +1,16 @@
 -- inserindo registros na tabela autor
 
 INSERT INTO autor (cod_autor,nome_autor,pais_autor)
-VALUES
-(1,'XXX','Brasil'),
-(2,'XXX','Brasil'),
-(3,'XXX','Brasil'),
-(4,'XXX','Brasil'),
-(5,'XXX','Brasil'),
-(6,'XXX','Brasil'),
-(7,'XXX','Brasil'),
-(8,'XXX','Brasil'),
-(9,'XXX','Brasil'),
-(10,'XXX','Brasil');
+	VALUES
+	(1,'XXX','Brasil'),
+	(2,'XXX','Brasil'),
+	(3,'XXX','Brasil'),
+	(4,'XXX','Brasil'),
+	(5,'XXX','Brasil'),
+	(6,'XXX','Brasil'),
+	(7,'XXX','Brasil'),
+	(8,'XXX','Brasil'),
+	(9,'XXX','Brasil'),
+	(10,'XXX','Brasil');
 
 		
