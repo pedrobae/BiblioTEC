8d947f219d079e0c1ba8c2237661213d98507b69
ClauMolinari <148367813+ClauMolinari@users.noreply.github.com>
2023-10-18 17:18:37 -0300

    Create situacao.sql

---
+++ situacao.sql
@@ -0,0 +1,14 @@
+
+
+insert into situacao (cod_situacao,descr_situacao)
+  values (1,'Disponível'),
+  (2,'Emprestado'),
+  (3,'Reservado'),
+  (4,'Reparo/Manutenção'),
+  (5,'Perdido'),
+  (6,'Danificado'),
+  (7,'Indisponível');
+
+
+  
+  
