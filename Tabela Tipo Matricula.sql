1c0316306eb537c88c06a3337094a0bce3c6d53b
ClauMolinari <148367813+ClauMolinari@users.noreply.github.com>
2023-10-18 17:27:00 -0300

    Create Tabela Tipo Matricula.sql

---
+++ Tabela Tipo Matricula.sql
@@ -0,0 +1,8 @@
+-- adicionando registros em tabela tipo_matricula
+
+insert into tipo_matricula (cod_tipo_matricula,descr_tipo_matricula)
+  values
+  (1,'Estudante'),
+  (2,'Professor'),
+  (3,'Funcionário),
+  (4,'Externo');
