1112717bcd7f419eccdea545d2ca1db2e737507e
ClauMolinari <148367813+ClauMolinari@users.noreply.github.com>
2023-10-18 18:34:54 -0300

    Update and rename Tabela Disciplina.sq; to Tabela Disciplina.sql

---
+++ Tabela Disciplina.sql
@@ -0,0 +1,52 @@
+-- inserir registros na  tabela disciplina
+
+
+INSERT INTO disciplina (cod_disciplina, descr_disciplina)
+  VALUES
+  (1,'Blockchain'),
+  (2,'Boas práticas de codificação'),
+  (3,'Criptomoedas'),
+  (4,'Desenvolvimento de software'),
+  (5,'Desenvolvimento pessoal'),
+  (6,'DevOps'),
+  (7,'Dicas'),
+  (8,'Educação financeira'),
+  (9,'Engenharia de software'),
+  (10,'Finanças pessoais'),
+  (11,'Hábitos'),
+  (12,'História da humanidade e da civilização'),
+  (13,'História da tecnologia e inovação'),
+  (14,'Inteligência artificial e suas aplicações'),
+  (15,'Introdução à programação'),
+  (16,'Investimentos'),
+  (17,'Mudança comportamental'),
+  (18,'Padrões de design'),
+  (19,'Programação'),
+  (20,'Programação em Python'),
+  (21,'Psicologia positiva'),
+  (22,'Técnicas para programadores'),
+  (23,'Tecnologia financeira'),
+  (24,'Tecnologia da informação');
+  
+
+
+
+
+
+
+
+
+"Clean Code: A Handbook of Agile Software Craftsmanship" -  e , .
+"The Pragmatic Programmer: Your Journey to Mastery" - Programação, ,  e .
+"Sapiens: Uma Breve História da Humanidade" - .
+"The Innovators: How a Group of Hackers, Geniuses, and Geeks Created the Digital Revolution" - 
+"Python Crash Course: A Hands-On, Project-Based Introduction to Programming" - , .
+"Design Patterns: Elements of Reusable Object-Oriented Software" - Programação e , .
+"The Phoenix Project: A Novel about IT, DevOps, and Helping Your Business Win" - , .
+"Inteligência Artificial: Como a tecnologia está mudando o mundo" - .
+"The Subtle Art of Not Giving a F*ck: A Counterintuitive Approach to Living a Good Life" - , .
+"A Revolução Bitcoin: como criptomoedas e a plataforma blockchain estão desafiando sistemas financeiros" - , e .
+"O Poder do Hábito: Por Que Fazemos o Que Fazemos na Vida e Nos Negócios" - ,  e .
+"Trabalhe 4 Horas por Semana" - Empreendedorismo, produtividade e estilo de vida.
+"Mindset: A Nova Psicologia do Sucesso" - Psicologia, desenvolvimento pessoal.
+"A Sutil Arte de Ligar o F*da-se: Uma Estratégia Inusitada Para Uma Vida Melhor" -.
