3132091e72f1fac751f9aa4b44f2c7e326edfc4b
pedrobae <pedrobettiolabe@gmail.com>
2023-10-19 10:47:23 -0300

    Update tabela instituicao.sql

--- tabela instituicao.sql
+++ tabela instituicao.sql
@@ -1,12 +1,10 @@
 -- criar tabela instituicao
-INSERT INTO instituicao VALUES ( '', 'Fatec Sao Paulo', 'Av. Tiradentes, 615 - Bom Retiro - Sao Paulo/SP', 'Fatec'); 
+INSERT INTO instituicao VALUES ( DEFAULT, 'Fatec Sao Paulo', 'Av. Tiradentes, 615 - Bom Retiro - Sao Paulo/SP', 'Fatec'); 
 
-INSERT INTO instituicao VALUES ( '', 'Fatec Pastor Eneas Tognini', 'R. Frei João, 59 - Vila Nair - Sao Paulo/SP', 'Fatec'); 
+INSERT INTO instituicao VALUES ( DEFAULT, 'Fatec Pastor Eneas Tognini', 'R. Frei João, 59 - Vila Nair - Sao Paulo/SP', 'Fatec'); 
 
-INSERT INTO instituicao VALUES ( '', 'Fatec SEBRAE', 'Alameda Nothman 598 - Campos Eliseos - Sao Paulo/SP', 'Fatec'); 
+INSERT INTO instituicao VALUES ( DEFAULT, 'Fatec SEBRAE', 'Alameda Nothman 598 - Campos Eliseos - Sao Paulo/SP', 'Fatec'); 
 
-INSERT INTO instituicao VALUES ( '', 'Etec Getulio Vargas', 'Rua Moreira e Costa, 243 - Ipiranga - Sao Paulo/SP', 'Etec'); 
+INSERT INTO instituicao VALUES ( DEFAULT, 'Etec Getulio Vargas', 'Rua Moreira e Costa, 243 - Ipiranga - Sao Paulo/SP', 'Etec'); 
 
-INSERT INTO instituicao VALUES ( '', 'Etec Lauro Gomes', 'Av. Pereira Barreto, 400 - Vila Baeta Neves - Sao Bernardo do Campo/SP', 'Etec'); 
-
-
+INSERT INTO instituicao VALUES ( DEFAULT, 'Etec Lauro Gomes', 'Av. Pereira Barreto, 400 - Vila Baeta Neves - Sao Bernardo do Campo/SP', 'Etec'); 
