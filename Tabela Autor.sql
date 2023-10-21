033e523b9b958428c5fac6ad7f5b796c7f576b6d
pedrobae <pedrobettiolabe@gmail.com>
2023-10-20 23:27:32 -0300

    Update Tabela Autor.sql

--- Tabela Autor.sql
+++ Tabela Autor.sql
@@ -5,12 +5,12 @@ INSERT INTO autor (cod_autor,nome_autor,pais_autor)
 	(1,'Robert C. Martin ','Estados Unidos'),
 	(2,'Andrew Hunt','Inglaterra'),
 	(3,'David Thomas','Inglaterra'),
-	(4,' Yuval Noah Harari','Israel'),
+	(4,'Yuval Noah Harari','Israel'),
 	(5,'Walter Isaacson','Estados Unidos'),
 	(6,'Eric Matthes','Estados Unidos'),
 	(7,'Erich Gamma','Suiça'),
 	(8,'Richard Helm','Estados Unidos'),
-	(9,' Ralph Johnson','Estados Unidos'),
+	(9,'Ralph Johnson','Estados Unidos'),
 	(10,'Gene Kim','Estados Unidos'),
 	(11,'Kevin Behr','Estados Unidos'),
 	(12,'George Spafford','Estados Unidos'),
@@ -20,24 +20,4 @@ INSERT INTO autor (cod_autor,nome_autor,pais_autor)
 	(16,'Charles Duhigg','Mexico'),
 	(17,'Timothy Ferriss','Estados Unidos'),
 	(18,'Carol S. Dweck ','Estados Unidos'),
-	(19,'Robert T. Kiyosaki','Estados Unidos'),
-	(20,'Mark Manson','Estados Unidos');
-	
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	(19,'Robert T. Kiyosaki','Estados Unidos');
