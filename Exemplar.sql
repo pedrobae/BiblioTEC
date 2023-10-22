086318a1ac4649f76ce1d8e97a374985fea61aad
pedrobae <pedrobettiolabe@gmail.com>
2023-10-22 17:59:55 -0300

    Create Exemplar.sql

---
+++ Exemplar.sql
@@ -0,0 +1,9 @@
+INSERT INTO exemplar (ISBN, dt_aquisicao, cod_istituicao, cod_estante, cod_situacao, estado_exemplar)
+        VALUES
+        (978-0132350884, 2023-10-22, 1, 1, 1, 'Preservado'),
+        (978-0132350884, 2023-2-22, 1, 1, 1, 'Danificado'),
+        (978-0132350884, 2023-10-22, 1, 1, 1, 'Preservado'),
+        (978-0137081073, 2019-12-14, 1, 2, 4, 'Danificado'),
+        (978-1491904240, 2015-4-1, 2, 1, 5, 'Preservado'),
+        (978-1491904240, 2017-4-10, 2, 1, 1, 'Preservado'),
+        (978-0345805852, 2018-10-26, 4, 3, 1, 'Danificado'),
