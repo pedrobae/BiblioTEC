b561959cef3b75827e61aaa4280b9e151306e36a
Pedro Abe <pedrobettiolabe@gmail.com>
2023-10-23 17:34:06 -0300

    Update Situacao.sql

--- Situacao.sql
+++ Situacao.sql
@@ -4,10 +4,8 @@ INSERT INTO situacao (cod_situacao,descr_situacao)
   VALUES
   (1,'Disponível'),
   (2,'Emprestado'),
-  (3,'Reservado'),
-  (4,'Reparo/Manutenção'),
-  (5,'Perdido'),
-  (6,'Indisponível');
+  (3,'Reparo/Manutenção'),
+  (4,'Perdido'),
 
 
   
