cf13bf3d41cf0ef7615b2a57484aa9f7e2c1c842
Pedro Abe <pedrobettiolabe@gmail.com>
2023-10-23 17:08:19 -0300

    Update Matricula.sql

--- Matricula.sql
+++ Matricula.sql
@@ -1,3 +1,3 @@
 INSERT INTO matricula(cod_matricula, cod_tipo_matricula, cod_instituicao, nome_matricula, dt_matricula, sexo, dt_nscm, email_matricula, endereco_matricula, CPF, dt_termino)
         VALUES
-        (
+        
