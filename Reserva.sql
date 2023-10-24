b5fb12bbbf0b3065d90f371d3eb22103e22a2bc9
ClauMolinari <148367813+ClauMolinari@users.noreply.github.com>
2023-10-24 14:24:26 -0300

    Update Reserva.sql

--- Reserva.sql
+++ Reserva.sql
@@ -1,3 +1,24 @@
 INSERT INTO emprestimo (cod_matricula, cod_exemplar, dt_reserva, dt_prevista_emprestimo)
-        VALUES
+          VALUES
+
+
+        (40101, 
+        (40111, 
+        (40102, 
+        (40121, 
+        (40103
+        (40113, 
+        (40112, 
+        (40122, 
+        (40123, 
+        (40131, 
+        (40104, 
+        (40105, 
+        (40115, 
+        (40114, 
+        (40124, 
+        (40134, 
+        (40125, 
+        (40135, 
+      
         
