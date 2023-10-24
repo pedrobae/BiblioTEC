4fb4cf18447b4b8bf5068e95a3ec579478366873
StephanieSayuri <148368004+StephanieSayuri@users.noreply.github.com>
2023-10-23 23:05:55 -0300

    Update Instituicao.sql

--- Instituicao.sql
+++ Instituicao.sql
@@ -1,8 +1,8 @@
 -- criar tabela instituicao
 INSERT INTO instituicao (cod_instituicao, nome_instituicao, endereco_instituicao, etec_fatec)
-  VALUES
-  (1, 'Fatec Sao Paulo', 'Av. Tiradentes, 615 - Bom Retiro - Sao Paulo/SP', 'Fatec'),
-  (2, 'Fatec Pastor Eneas Tognini', 'R. Frei João, 59 - Vila Nair - Sao Paulo/SP', 'Fatec'), 
-  (3, 'Fatec SEBRAE', 'Alameda Nothman 598 - Campos Eliseos - Sao Paulo/SP', 'Fatec'),
-  (4, 'Etec Getulio Vargas', 'Rua Moreira e Costa, 243 - Ipiranga - Sao Paulo/SP', 'Etec'),
-  (5, 'Etec Lauro Gomes', 'Av. Pereira Barreto, 400 - Vila Baeta Neves - Sao Bernardo do Campo/SP', 'Etec');
+        VALUES
+        (1, 'Fatec Sao Paulo', 'Av. Tiradentes, 615 - Bom Retiro - Sao Paulo/SP', 'Fatec'),
+        (2, 'Fatec Pastor Eneas Tognini', 'R. Frei João, 59 - Vila Nair - Sao Paulo/SP', 'Fatec'), 
+        (3, 'Fatec SEBRAE', 'Alameda Nothman 598 - Campos Eliseos - Sao Paulo/SP', 'Fatec'),
+        (4, 'Etec Getulio Vargas', 'Rua Moreira e Costa, 243 - Ipiranga - Sao Paulo/SP', 'Etec'),
+        (5, 'Etec Lauro Gomes', 'Av. Pereira Barreto, 400 - Vila Baeta Neves - Sao Bernardo do Campo/SP', 'Etec');
