01f42f4d00203656ecd08666a9005884fc3e0e2f
Pedro Abe <pedrobettiolabe@gmail.com>
2023-10-25 14:53:34 -0300

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,9 @@
+# Registro de Exemplar
+
+# Registro de Matricula
+
+# Realizar Emprestimo
+
+# Realizar Reserva (definir a ordem para calcular o data de emprestimo prevista)
+
+# Transformar Reserva em Emprestimo
