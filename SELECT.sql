1afb09206e3f29a62a3c6c37bdce65f244bc7d03
Pedro Abe <pedrobettiolabe@gmail.com>
2023-10-31 16:30:14 -0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,12 +1,12 @@
 -- Livros atrasados e quem os emprestou
-SELECT titulo, nome_matricula, dt_devolucao - dt_prevista_devolucao AS "Dias Atrasados"
+SELECT nome_matricula, titulo, dt_devolucao - dt_prevista_devolucao AS "Dias Atrasados"
     FROM emprestimo emp JOIN matricula mat
         ON (emp.cod_matricula = mat.cod_matricula)
                         JOIN exemplar ex
         ON (emp.cod_exemplar = ex.cod_exemplar)
                         JOIN livro liv
         ON (ex.ISBN = liv.ISBN)
-            WHERE emp.dt_devolucao > emp.dt_prevista_devolucao;
-
+            WHERE emp.dt_devolucao > emp.dt_prevista_devolucao
+                ORDER BY "Dias Atrasados" DESC;
 
 -- Lista de reserva de um exemplar e seus e-mail
