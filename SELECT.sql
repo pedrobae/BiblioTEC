f8187a032410b623e91c065a4c7c8a2bdc09aa3a
Pedro Abe <pedrobettiolabe@gmail.com>
2023-10-31 18:23:11 -0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -10,3 +10,12 @@ SELECT nome_matricula, titulo, dt_devolucao - dt_prevista_devolucao AS "Dias Atr
                 ORDER BY "Dias Atrasados" DESC;
 
 -- Lista de reserva de um exemplar e seus e-mail
+SELECT nome_matricula, email_matricula, dt_prevista_emprestimo, titulo
+    FROM reserva res JOIN matricula mat
+        ON (res.cod_matricula = mat.cod_matricula)
+                     JOIN exemplar ex
+        ON (res.cod_exemplar = ex.cod_exemplar)
+                        JOIN livro liv
+        ON (ex.ISBN = liv.ISBN)
+            WHERE res.dt_emprestimo = NULL
+                ORDER BY dt_prevista_emprestimo;
