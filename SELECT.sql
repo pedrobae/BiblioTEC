e3b66a477982819bea139a3451651bed28f4eb7b
StephanieSayuri <148368004+StephanieSayuri@users.noreply.github.com>
2023-11-08 10:38:56 -0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,21 +1,34 @@
 -- Livros atrasados e quem os emprestou
-SELECT nome_matricula, titulo, dt_devolucao - dt_prevista_devolucao AS "Dias Atrasados"
+SELECT mat.nome_matricula, liv.titulo, emp.dt_devolucao - emp.dt_prevista_devolucao AS "Dias Atrasados"
     FROM emprestimo emp JOIN matricula mat
         ON (emp.cod_matricula = mat.cod_matricula)
                         JOIN exemplar ex
         ON (emp.cod_exemplar = ex.cod_exemplar)
                         JOIN livro liv
         ON (ex.ISBN = liv.ISBN)
-            WHERE emp.dt_devolucao > emp.dt_prevista_devolucao
-                ORDER BY "Dias Atrasados" DESC;
+    WHERE emp.dt_devolucao > emp.dt_prevista_devolucao
+    ORDER BY "Dias Atrasados" DESC;
 
 -- Lista de reserva de um exemplar e seus e-mail
-SELECT nome_matricula, email_matricula, dt_prevista_emprestimo, titulo
+SELECT mat.nome_matricula, mat.email_matricula, res.dt_prevista_emprestimo, liv.titulo
     FROM reserva res JOIN matricula mat
         ON (res.cod_matricula = mat.cod_matricula)
                      JOIN exemplar ex
         ON (res.cod_exemplar = ex.cod_exemplar)
-                        JOIN livro liv
+                     JOIN livro liv
         ON (ex.ISBN = liv.ISBN)
-            WHERE res.dt_emprestimo IS NULL AND res.cod_exemplar = 'INSERIR CODIGO DO EXEMPLAR'
-                ORDER BY dt_prevista_emprestimo;
+    WHERE res.dt_emprestimo IS NULL AND res.cod_exemplar = 'INSERIR CODIGO DO EXEMPLAR'
+    ORDER BY dt_prevista_emprestimo;
+
+-- Consulta de livros emprestados
+SELECT liv.ISBN, liv.titulo, exe.cod_exemplar, mat.cod_matricula, mat.nome, emp.dt_emprestimo, emp.dt_prevista_devolucao
+    FROM livro liv JOIN exemplar exe
+        ON (liv.ISBN = exe.ISBN)
+                   JOIN emprestimo emp
+        ON (exe.cod_exemplar = emp.cod_exemplar)
+                   JOIN matricula mat
+        ON (mat.cod_matricula = emp.cod_matricula)
+    WHERE emp.dt_devolucao IS NULL;
+                    
+
+-- Consultar histórico de aluno/ professor / funcionário
