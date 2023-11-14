565403c6f1d394e0716b5f95099ebdc5dbd38bf3
Pedro Abe <pedrobettiolabe@gmail.com>
2023-11-14 17:51:42 -0300

    Update display_acervo.py

--- Python/display_acervo.py
+++ Python/display_acervo.py
@@ -4,31 +4,49 @@ from func_acervo import acervo_disp, acervo_empr, acervo_manu, acervo_perd
 def display_disp():
     acervo = acervo_disp()
     layout = [
-        sg.Table(acervo, ['Código do Exemplar','Titulo','Data de Aquisição'])
+        [sg.Table(acervo, ['Código do Exemplar','Titulo','Data de Aquisição'])]
         ]
     window = sg.Window('Acervo Disponível', layout)
-    sg.PopupScrolled(window)
+    while True:
+        evento, valores = window.read()
+        if evento == sg.WIN_CLOSED:
+            break
+
+
 
 def display_empr():
     acervo = acervo_empr()
     layout = [
-        sg.Table(acervo, ['Código do Exemplar','Titulo','Matricula do Empréstimo', 'Data Prevista de Devolução'])
+        [sg.Table(acervo, ['Código do Exemplar','Titulo','Matricula do Empréstimo', 'Data Prevista de Devolução'])]
         ]
     window = sg.Window('Acervo Emprestado', layout)
-    sg.PopupScrolled(window)
+    while True:
+        evento, valores = window.read()
+        if evento == sg.WIN_CLOSED:
+            break
+
+
 
 def display_manu():
     acervo = acervo_manu()
     layout = [
-        sg.Table(acervo, ['Código do Exemplar','Titulo'])
+        [sg.Table(acervo, ['Código do Exemplar','Titulo'])]
         ]
     window = sg.Window('Acervo em Manutenção', layout)
-    sg.PopupScrolled(window)
+    while True:
+        evento, valores = window.read()
+        if evento == sg.WIN_CLOSED:
+            break
+
+
 
 def display_perd():
     acervo = acervo_perd()
     layout = [
-        sg.Table(acervo, ['Código do Exemplar','Titulo','Matricula do Último Empréstimo'])
+        [sg.Table(acervo, ['Código do Exemplar','Titulo','Matricula do Último Empréstimo'])]
         ]
     window = sg.Window('Acervo Perdido', layout)
-    sg.PopupScrolled(window)
+    while True:
+        evento, valores = window.read()
+        if evento == sg.WIN_CLOSED:
+            break
