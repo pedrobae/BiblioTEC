25b89f8774cf8a94e39ec60255fe01cc75f1afd6
Pedro Abe <pedrobettiolabe@gmail.com>
2023-11-23 13:22:47 -0300

    resgistra_autor

--- Python/func_cadastro.py
+++ Python/func_cadastro.py
@@ -19,7 +19,42 @@ def nome_da_função (coluna1, coluna2, ..., colunaN):
 
 
 # Registro de Autor
+def registra_autor (cod_autor, nome_autor, pais_autor):
+    retorno = None
+    # Conecta com o banco
+    con = None
+    try:
+        with psycopg2.connect(
+                        database = "BiblioTEC", 
+                        user = "postgres", 
+                        password = "123456", 
+                        host = "localhost",
+                        port = "5432") as con:
+        
+            with con.cursor() as cur:
+                # Seleciona os codigos de autor existentes e coloca em uma lista
+                select_aut = "SELECT cod_autor FROM autor"
+                cur.execute(select_aut)
+                lista_aut = []
+                for value in cur.fetchall():
+                    lista_aut += value
 
+                # Checa se o cod_autor existe na lista
+                if cod_autor in lista_aut:
+                    retorno = 'O Código de Autor ja está cadastrado'
+                else:
+                    # Realiza o cadastro
+                    insert_aut = "INSERT INTO autor VALUES ({0}, '{1}', '{2}')".format(cod_autor, nome_autor, pais_autor)
+                    cur.execute(insert_aut)
+                    retorno = 'Cadastro Efetuado'
+
+    except Exception as error:
+        print(error)
+    finally:
+        if con is not None:
+            con.close()
+    if retorno:
+        return retorno
 
 
 
@@ -44,3 +79,6 @@ finally:
     if con is not None:
         con.close()
 '''
+
+if __name__ == "__main__":
+    registra_autor(30, 'Brandon Sanderson', 'Estados Unidos')
