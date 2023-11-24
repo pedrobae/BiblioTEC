a9b158607ac450e6c09d5669fbc556d82c4254d9
Pedro Abe <pedrobettiolabe@gmail.com>
2023-11-24 19:30:03 -0300

    janela livro, mat, autor

--- Python/window_acervo.py
+++ Python/window_acervo.py
@@ -1,8 +1,8 @@
 import PySimpleGUI as sg
-from func_acervo import acervo_disp, acervo_empr, acervo_manu, acervo_perd
+import func_acervo as fa
 
 def wind_disp():
-    acervo = acervo_disp()
+    acervo = fa.acervo_disp()
     layout = [
         [sg.Table(acervo, ['Código do Exemplar','Titulo','Data de Aquisição'])]
         ]
@@ -11,7 +11,7 @@ def wind_disp():
 
 
 def wind_empr():
-    acervo = acervo_empr()
+    acervo = fa.acervo_empr()
     layout = [
         [sg.Table(acervo, ['Código do Exemplar','Titulo','Matricula do Empréstimo', 'Data Prevista de Devolução'])]
         ]
@@ -20,7 +20,7 @@ def wind_empr():
 
 
 def wind_manu():
-    acervo = acervo_manu()
+    acervo = fa.acervo_manu()
     layout = [
         [sg.Table(acervo, ['Código do Exemplar','Titulo'])]
         ]
@@ -28,8 +28,32 @@ def wind_manu():
 
 
 def wind_perd():
-    acervo = acervo_perd()
+    acervo = fa.acervo_perd()
     layout = [
         [sg.Table(acervo, ['Código do Exemplar','Titulo','Matricula do Último Empréstimo'])]
         ]
     return sg.Window('Acervo Perdido', layout, finalize=True)
+
+
+def wind_liv():
+    acervo = fa.acervo_liv()
+    layout = [
+        [sg.Table(acervo, ['ISBN','Titulo','Subtitulo','dt_publ','editora','local_publ'])]
+        ]
+    return sg.Window('Livros Registrados', layout, finalize=True)
+
+
+def wind_mat():
+    acervo = fa.acervo_mat()
+    layout = [
+        [sg.Table(acervo, ['cod_matricula', 'descr_matricula', 'nome_matricula', 'sexo', 'dt_nscm', 'dt_termino'])]
+        ]
+    return sg.Window('Matriculas Registradas', layout, finalize=True)
+
+
+def wind_aut():
+    acervo = fa.acervo_aut()
+    layout = [
+        [sg.Table(acervo, ['cod_autor', 'nome_autor', 'pais_autor'])]
+        ]
+    return sg.Window('Autores Registrados', layout, finalize=True)
