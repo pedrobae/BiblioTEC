84aaef78522b08fad9e100856f725e18c11df33d
Pedro Abe <pedrobettiolabe@gmail.com>
2023-11-24 15:11:57 -0300

    reestruturação

--- Python/display_cadastro.py
+++ Python/display_cadastro.py
@@ -1,32 +1,50 @@
 import PySimpleGUI as sg
 import func_acervo as fa
 import func_cadastro as fc
+import display_menu as dm
 
 # Função que gera a janela das matriculas
 def window_mat():
+    
     # Layout
     layout = [
         [sg.Text("Código de Matricula: "), sg.Input(size=(10, 1), key = '-COD_MAT-')],
         [sg.Text("")]
         [sg.Button()]
     ]
+
     # Gera a Janela e retorna
     return sg.Window(title= "Matrícula", layout = layout, size = ())
     
 # Função que cria o display e realiza as operações
 def display_mat():
-    # Gera as janelas
-    window_mat = window_mat()
+
+    # Abro a janela
+    window = window_mat()
+
     # Loop de leitura da tela
     while True:
-        window, evento, valores = sg.read_all_windows()
+        evento, valores = window.read()
+
         # Evento de fechamento de tela
         if evento == sg.WIN_CLOSED():
-            window.close()
+            open = 'menu'
+            break
+
         # Evento de cadastro
-        if evento == '-CADASTRO-':
+        elif evento == '-CADASTRO-':
             output = fc.registra_matricula(valores['-COD_MAT-'], valores['-NOME_MAT-'])
             sg.popup(output)
         # Evento de atualização
 
+
         # Evento de mostrar as matrículas
+
+
+    # Fecho a Janela
+    window.close()
+
+    # Abro o Menu
+    if open == 'menu':
+        dm.display_menu()
+
