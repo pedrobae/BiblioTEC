42ecf0e381660a7eed4596c2d7e16e7f8f4fe6a4
Pedro Abe <pedrobettiolabe@gmail.com>
2023-11-26 19:44:18 -0300

    ste - window_mat

--- Python/display_cadastro.py
+++ Python/display_cadastro.py
@@ -3,18 +3,24 @@ from Funcoes import func_acervo as fa
 from Funcoes import func_cadastro as fc
 import display_menu as dm
 
-# Função que gera a janela das matriculas
 def window_mat():
     
     # Layout
     layout = [
         [sg.Text("Código de Matricula: "), sg.Input(size=(10, 1), key = '-COD_MAT-')],
-        [sg.Text("")]
-        [sg.Button()]
+        [sg.Text("Tipo de Matrícula: "), sg.Input(size=(18, 1), key = '-TIPO_MAT-')],
+        [sg.Text("Instituição: "), sg.Input(size=(18, 1), key = '-INST_MAT-')],
+        [sg.Text("Sexo: "), sg.Input(size=(18, 1), key = '-SEXO_MAT-')],
+        [sg.Text("Data de Nascimento: "), sg.Input(size=(10, 1), key = '-NASC_MAT-')],
+        [sg.Text("E-mail: "), sg.Input(size=(25, 1), key = '-EMAIL_MAT-')],
+        [sg.Text("Endereço: "), sg.Input(size=(50, 1), key = '-ENDE_MAT-')],
+        [sg.Text("CPF: "), sg.Input(size=(12, 1), key = '-CPF_MAT-')],
+        [sg.Button('Cadastrar', size = (40), key = '-CADASTRO-')],
+        [sg.Button('Atualizar', size = (40), key = '-ATUALIZA-')]
     ]
 
     # Gera a Janela e retorna
-    return sg.Window(title= "Matrícula", layout = layout, size = ())
+    return sg.Window(title= "Matrícula", layout = layout, size = (600, 300))
     
 # Função que cria o display e realiza as operações
 def display_mat():
@@ -33,18 +39,20 @@ def display_mat():
 
         # Evento de cadastro
         elif evento == '-CADASTRO-':
-            output = fc.registra_matricula(valores['-COD_MAT-'], valores['-NOME_MAT-'])
+            output = fc.registra_matricula(valores['-COD_MAT-'], valores['-TIPO_MAT-'], valores ['-INST_MAT-'], valores ['-SEXO_MAT-'], valores ['-NASC_MAT-'], valores ['-EMAIL_MAT-'], valores ['-ENDE_MAT-'], valores ['-CPF_MAT-'])
             sg.popup(output)
-        # Evento de atualização
 
+        # Evento de atualizar
+        elif evento == '-ATUALIZA-':
+            output2 = fc.atualiza_matricula(valores['-COD_MAT-'], valores['-TIPO_MAT-'], valores ['-INST_MAT-'], valores ['-SEXO_MAT-'], valores ['-NASC_MAT-'], valores ['-EMAIL_MAT-'], valores ['-ENDE_MAT-'], valores ['-CPF_MAT-'])
+            sg.popup(output2)
 
-        # Evento de mostrar as matrículas
-
-
-    # Fecho a Janela
+    # Fechar janela
     window.close()
 
-    # Abro o Menu
     if open == 'menu':
         dm.display_menu()
+
+if __name__ == "__main__":
+    display_mat()
 
