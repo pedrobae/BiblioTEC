11949ed8dea068121c0ad6e8884a8db7d8e9f8eb
Beatriz Oyadomari <be-oyadomari@uol.com.br>
2023-12-01 12:53:38 -0300

    1

--- Banco de Dados/SELECT.sql
+++ Banco de Dados/SELECT.sql
@@ -1,6 +1,6 @@
 -- Active: 1701269989672@@127.0.0.1@5432@BiblioTEC
 -- Livros atrasados e quem os emprestou
-SELECT mat.nome_matricula, liv.titulo, emp.dt_devolucao - emp.dt_prevista_devolucao AS "Dias Atrasados"
+SELECT mat.nome_matricula, liv.titulo, EXTRACT(day FROM (emp.dt_devolucao - emp.dt_prevista_devolucao)) AS "Dias Atrasados"
     FROM emprestimo emp 
         JOIN matricula mat      ON (emp.cod_matricula = mat.cod_matricula)
         JOIN exemplar ex        ON (emp.cod_exemplar = ex.cod_exemplar)
@@ -14,11 +14,11 @@ SELECT mat.nome_matricula, mat.email_matricula, res.dt_prevista_emprestimo, liv.
         JOIN matricula mat      ON (res.cod_matricula = mat.cod_matricula)
         JOIN exemplar ex        ON (res.cod_exemplar = ex.cod_exemplar)
         JOIN livro liv          ON (ex.ISBN = liv.ISBN)
-    WHERE res.dt_emprestimo IS NULL AND res.cod_exemplar = 'INSERIR CODIGO DO EXEMPLAR'
+    WHERE res.dt_emprestimo IS NULL AND res.cod_exemplar = 2
     ORDER BY dt_prevista_emprestimo;
 
 -- Consulta de livros emprestados
-SELECT liv.ISBN, liv.titulo, exe.cod_exemplar, mat.cod_matricula, mat.nome, emp.dt_emprestimo, emp.dt_prevista_devolucao
+SELECT liv.ISBN, liv.titulo, exe.cod_exemplar, mat.cod_matricula, mat.nome_matricula, emp.dt_emprestimo, emp.dt_prevista_devolucao
     FROM livro liv 
         JOIN exemplar exe       ON (liv.ISBN = exe.ISBN)
         JOIN emprestimo emp     ON (exe.cod_exemplar = emp.cod_exemplar)
@@ -31,7 +31,7 @@ SELECT mat.nome_matricula, mat.cod_tipo_matricula, ex.cod_exemplar, liv.titulo, 
         JOIN emprestimo emp     ON (emp.cod_matricula = mat.cod_matricula)
         JOIN exemplar ex        ON (ex.cod_exemplar= emp.cod_exemplar)
         JOIN livro liv          ON (ex.ISBN = liv.ISBN )
-    WHERE emp.cod_matricula ='INSERIR CODIGO DE MATRICULA'
+    WHERE emp.cod_matricula = 40102
     ORDER BY dt_emprestimo DESC
 
 -- Consultar os 5 livros mais emprestados
@@ -47,8 +47,3 @@ SELECT m.cod_matricula, m.nome_matricula
     FROM matricula m
         LEFT JOIN emprestimo em ON m.cod_matricula = em.cod_matricula
     WHERE em.cod_matricula IS NULL
-
-    SELECT m.cod_matricula, tm.descr_tipo_matricula, m.nome_matricula, m.sexo, m.dt_nscm, m.dt_termino
-                                        FROM matricula m
-                                            JOIN tipo_matricula tm      ON m.cod_tipo_matricula = tm.cod_tipo_matricula
-                                        ORDER BY nome_matricula
